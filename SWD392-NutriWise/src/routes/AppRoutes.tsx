c5d6ed61522b6893b6ad18decf9114c1f26818f2
TLChouny <103498836+TLChouny@users.noreply.github.com>
2025-02-18 21:15:51 +0700

    up approute

--- SWD392-NutriWise/src/routes/AppRoutes.tsx
+++ SWD392-NutriWise/src/routes/AppRoutes.tsx
@@ -2,20 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/Home/HomePage";
 import AdminPage from "../pages/Admin/AdminPage";
-{
-  /* Đảm bảo dùng đúng trang này */
-}
-
 
 const AppRoutes: React.FC = () => {
   return (
-    <Router>
+    <Router basename={process.env.REACT_APP_BASE_URL || '/'}>
       <Routes>
-        <Route path='/' element={<HomePage/>} />
-        <Route path='/admin' element={<AdminPage/>} />
-
-        
-                </Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
     </Router>
   );
 };
