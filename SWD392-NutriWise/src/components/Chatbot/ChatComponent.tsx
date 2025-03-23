915b5cfb4a02c0e053b61e81e7fe955f0a600109
nam127 <buithanhnam9109@gmail.com>
2025-03-24 00:03:44 +0700

    feat: send message, render chat sessions

--- SWD392-NutriWise/src/components/Chatbot/ChatComponent.tsx
+++ SWD392-NutriWise/src/components/Chatbot/ChatComponent.tsx
@@ -1,11 +1,14 @@
 import apiClient from "../../api/apiClient";
-import { ChatMessageDTO } from "../../types/types";
+import { ChatMessageDTO, ChatSessionDTO } from "../../types/types";
 import { Stack, Button, Text, Textarea, Box } from "@chakra-ui/react";
 import ArrowUpwardOutlinedIcon from '@mui/icons-material/ArrowUpwardOutlined';
-import { useCallback, useEffect, useState } from "react";
+import dayjs from "dayjs";
+import { useCallback, useEffect, useState, useRef } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Toast } from "../ToastComponent";
 
 interface ChatProps {
-    sessionId: number
+    setSessions: React.Dispatch<React.SetStateAction<ChatSessionDTO[]>>
 }
 
 const initialState: ChatMessageDTO = {
@@ -16,21 +19,32 @@ const initialState: ChatMessageDTO = {
     content: ''
 }
 
-export const Chat = ({ sessionId }: ChatProps) => {
+export const Chat = ({ setSessions }: ChatProps) => {
+    const userId = localStorage.getItem('userId');
+    // const userId = 1;
+    
+    const messagesEndRef = useRef<HTMLDivElement>(null);
+    const navigate = useNavigate();
+
+    const { sessionId } = useParams();
+    
     const [messages, setMessages] = useState<ChatMessageDTO[]>([initialState]);
     const [prompt, setPrompt] = useState<string>('');
-
-    // const userId = localStorage.getItem('userId');
-    const userId = 1;
+    const [toastInfo, setToastInfo] = useState<{ open: boolean; message: string }>({
+        open: false,
+        message: "",
+    });
 
     const fetchMessages = useCallback(async () => {
         if (sessionId) {
             try {
                 const response = await apiClient.get(`/Chat/session/${sessionId}?includeMessages=true`);
+                console.log('session chat component: ', response.data);
                 setMessages(response.data.messages);
             } catch (error) {
                 console.error("Failed to fetch messages", error);
             }
+
         } else {
             setMessages([]);
         }
@@ -38,48 +52,64 @@ export const Chat = ({ sessionId }: ChatProps) => {
 
 
     useEffect(() => {
-        fetchMessages();
-    }, [fetchMessages])
+        if (sessionId) {
+            fetchMessages();
+            console.log('messages sent chat component: ', messages);
+        }
+    }, [sessionId, fetchMessages])
 
-    const handleSend = async (content: string) => {
+    useEffect(() => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, [messages]);
+
+    const handleSend = useCallback(async () => {
+        if (!prompt.trim()) return;
+        const content = prompt;
+        console.log(content);
+        setPrompt(() => '');
         try {
+            let newSessionId = Number(sessionId ?? 0);
+
             if (!sessionId) {
                 const response = await apiClient.post(`/Chat/session`, {
                     userId: userId,
-                    title: 'New conversation'
+                    title: `New chat ${dayjs(new Date())}`
                 })
+                setSessions((prev) => [response.data, ...prev]);
+                newSessionId = response.data.chatSessionId;
 
-                sessionId = response.data.chatSessionId;
+                navigate(`${newSessionId}`);
             }
 
-            const responseMessage = await apiClient.post(`/Chat/message`, {
-                chatSessionId: sessionId,
+            const responseMessage = await apiClient.post(`/Chat/message?userId=${userId}`, {
+                chatSessionId: newSessionId,
                 content: content
             })
 
-            if (responseMessage.status === 200) {
-                const userMessage = responseMessage.data.userMessage;
-                const aiMessage = responseMessage.data.aiResponse;
+            // if (responseMessage.status === 200) {
+            const userMessage = responseMessage.data.userMessage;
+            const aiMessage = responseMessage.data.aiResponse;
 
-                setMessages([...messages, {
-                    chatSessionId: sessionId,
-                    chatMessageId: userMessage.chatMessageId,
-                    isUserMessage: userMessage.isUserMessage,
-                    sentTime: userMessage.sentTime,
-                    content: userMessage.content
-                },{
-                    chatSessionId: sessionId,
-                    chatMessageId: aiMessage.chatMessageId,
-                    isUserMessage: aiMessage.isUserMessage,
-                    sentTime: aiMessage.sentTime,
-                    content: aiMessage.content
-                }])
-                
-            }
+            setMessages([...messages, {
+                chatSessionId: newSessionId,
+                chatMessageId: userMessage.chatMessageId,
+                isUserMessage: userMessage.isUserMessage,
+                sentTime: userMessage.sentTime,
+                content: userMessage.content
+
+            }, {
+                chatSessionId: newSessionId,
+                chatMessageId: aiMessage.chatMessageId,
+                isUserMessage: aiMessage.isUserMessage,
+                sentTime: aiMessage.sentTime,
+                content: aiMessage.content
+
+            }]);
+            // }
         } catch (err) {
-            console.error(err)
+            setToastInfo({ open: true, message: "NutriWise AI đang có lỗi ><" });
         }
-    }
+    }, [prompt, sessionId, setSessions, navigate])
 
     return (
         <Stack width="full" height="full" p={4}>
@@ -101,11 +131,14 @@ export const Chat = ({ sessionId }: ChatProps) => {
                             borderRadius={15}
                             p={3}
                             bg={msg.isUserMessage ? 'gray.100' : ''}
+                            whiteSpace="pre-wrap"
+                            wordBreak="break-word"
                         >
                             {msg.content}
                         </Box>
                     ))
                 ) : null}
+                <div ref={messagesEndRef} />
             </Stack>
 
             <Stack width="full"
@@ -134,12 +167,29 @@ export const Chat = ({ sessionId }: ChatProps) => {
                     borderStyle={"groove"}
                     padding={"20px 20px 70px 20px"}
                 >
-                    <Textarea autoresize maxH="1hz" placeholder="Hỏi bất kì điều gì về thực đơn..." variant={"flushed"} onChange={(e) => setPrompt(e.target.value)} />
-                    <Button bgColor={"blue.700"} borderRadius={15} onClick={() => handleSend(prompt)}>
+                    <Textarea
+                        value={prompt}
+                        autoresize maxH="40"
+                        placeholder="Hỏi bất kì điều gì về thực đơn..."
+                        variant={"flushed"}
+                        onChange={(e) => setPrompt(e.target.value)}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter" && !e.shiftKey) {
+                                e.preventDefault(); // Prevents new line
+                                handleSend(); // Calls the send method
+                            }
+                        }} />
+                    <Button bgColor={"blue.700"} borderRadius={15} onClick={handleSend}>
                         <ArrowUpwardOutlinedIcon fontSize="medium" />
                     </Button>
                 </Stack>
             </Stack>
+            <Toast
+                open={toastInfo.open}
+                onClose={() => setToastInfo({ open: false, message: "" })}
+                information={toastInfo.message}
+                statusCode={500}
+            />
         </Stack >
     );
 };
--- SWD392-NutriWise/src/components/Chatbot/ChatSidebarComponent.tsx
+++ SWD392-NutriWise/src/components/Chatbot/ChatSidebarComponent.tsx
@@ -1,97 +1,126 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { Button, VStack, Collapsible, Stack, Text, Spacer } from "@chakra-ui/react";
+import React, { useCallback, useEffect, useState, useRef } from "react";
+import { Button, VStack, Collapsible, Stack, Text, Spacer, Spinner } from "@chakra-ui/react";
 import AutoAwesomeMosaicIcon from '@mui/icons-material/AutoAwesomeMosaic';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import { HeartPulse, Trash } from "lucide-react";
 import apiClient from "../../api/apiClient";
 import { useNavigate } from "react-router-dom";
 import { Box, Divider, Modal } from "@mui/material";
+import { ChatSessionDTO } from "../../types/types";
 
 interface ChatSidebarProps {
     isOpen: boolean;
     setIsOpen: (isOpen: boolean) => void;
-    userId: number,
+    loading: boolean
+    hasMore: boolean
+    sessions: ChatSessionDTO[],
+    handleScroll: (e: React.UIEvent<HTMLDivElement>) => void
 }
 
-interface ChatSession {
-    chatSessionId: number,
-    title: string,
-    lastUpdatedDate: string,
-}
+export const ChatSidebar = ({
+    isOpen,
+    setIsOpen,
+    loading,
+    sessions,
+    handleScroll }: ChatSidebarProps) => {
 
-export const ChatSidebar = ({ isOpen, setIsOpen, userId }: ChatSidebarProps) => {
+    const [open, setOpen] = useState<boolean>(false);
+    const [sessionId, setSessionId] = useState<number>();
+
     const navigate = useNavigate();
 
-    const [sessions, setSessions] = useState<ChatSession[]>([]);
-    const [pageNumber, setPageNumber] = useState<number>(1);
-    const [open, setOpen] = useState<boolean>(false);
-
-    const fetchSessions = useCallback(async () => {
-        const response = await apiClient.get(`/Chat/user/${userId}?PageNumber=${pageNumber}&PageSize=${15}&OrderBy=lastUpdatedDate`);
-        setSessions(response.data)
-    }, [userId, pageNumber]);
-
-    useEffect(() => {
-        fetchSessions();
-    }, [fetchSessions])
-
     return (
-        <Collapsible.Root
-            position={"absolute"}
-            open={isOpen}
-            onClick={() => setIsOpen(!isOpen)}>
-            <Collapsible.Trigger paddingX={isOpen ? 255 : 0} cursor="button">
-                <AutoAwesomeMosaicIcon
-                    color="primary"
-                    fontSize={"large"} />
-            </Collapsible.Trigger>
-            <Collapsible.Content onClick={(e) => e.stopPropagation()}>
-                <VStack
-                    align="stretch"
-                    position="fixed"
-                    left={0}
-                    top={0}
-                    height="full"
-                    width={isOpen ? "250px" : "0px"}
-                    bg="whiteAlpha.600"
-                    color="black"
-                    p={isOpen ? 4 : 0}
-                    shadow="md"
-                >
-                    <Stack direction="column" gap={10}>
-                        <Stack direction="row" justifyContent="flex-end" onClick={() => navigate(`/`)}>
-                            <Button variant={"ghost"} color="blue" size={"lg"}>
-                                <HeartPulse />
-                                NutriWise
-                            </Button>
-                            <Button variant="ghost" width={45} height={45} borderRadius={40}>
-                                <SearchOutlinedIcon />
-                            </Button>
+        <>
+            <Collapsible.Root
+                position={"absolute"}
+                open={isOpen}
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <Collapsible.Trigger paddingX={isOpen ? 255 : 0} cursor="button">
+                    <AutoAwesomeMosaicIcon
+                        color="primary"
+                        fontSize={"large"} />
+                </Collapsible.Trigger>
+                <Collapsible.Content onClick={(e) => e.stopPropagation()}>
+                    <VStack
+                        // ref={listRef}
+                        align="stretch"
+                        position="fixed"
+                        left={0}
+                        top={0}
+                        height="full"
+                        width={isOpen ? "250px" : "0px"}
+                        bg="whiteAlpha.600"
+                        color="black"
+                        p={isOpen ? 4 : 0}
+                        shadow="md"
+                        overflowY="auto"
+                        onScroll={handleScroll}
+                    >
+                        <Stack direction="column" gap={10}>
+                            <Stack direction="row" justifyContent="flex-end" onClick={() => navigate(`/`)}>
+                                <Button variant={"ghost"} color="blue" size={"lg"}>
+                                    <HeartPulse />
+                                    NutriWise
+                                </Button>
+                                <Button variant="ghost" width={45} height={45} borderRadius={40}>
+                                    <SearchOutlinedIcon />
+                                </Button>
+                            </Stack>
+                            <Button variant="ghost" onClick={() => {
+                                navigate(`/nutriwise/chats`);
+                            }}>Tạo đoạn chat mới</Button>
+                            <Stack>
+                                {sessions.map((session) => (
+                                    <>
+                                        <Button
+                                            key={session.chatSessionId}
+                                            variant="ghost"
+                                            onClick={() => navigate(`/nutriwise/chats/${session.chatSessionId}`)}
+                                            display="flex"
+                                            justifyContent="space-between"
+                                            width="100%"
+                                        >
+                                            <Text color="blackAlpha.800"
+                                                whiteSpace="nowrap"
+                                                overflow="hidden"
+                                                textOverflow="ellipsis"
+                                                maxWidth="80%"
+                                            >
+                                                {session.title}
+                                            </Text>
+                                            <Spacer />
+                                            <Trash color="red" onClick={(e) => {
+                                                e.stopPropagation();
+                                                setSessionId(session.chatSessionId);
+                                                setOpen(true);
+                                            }} />
+                                        </Button>
+
+                                    </>
+                                ))}
+                                {loading && (
+                                    <Box display="flex" justifyContent="center" my={4}>
+                                        <Spinner
+                                            borderWidth="4px"
+                                            alignSelf="center"
+                                            color="blue.500"
+                                            animationDuration="0.8s"
+                                            size="lg"
+                                        />
+                                    </Box>
+                                )}
+                            </Stack>
                         </Stack>
-                        <Button variant="ghost" onClick={() => navigate(`/nutriwise/chats`)}>Tạo đoạn chat mới</Button>
-                        <Stack>
-                            {sessions.map((session) => (
-                                <>
-                                    <Button key={session.chatSessionId} variant="ghost" onClick={() => navigate(`/nutriwise/chats/${session.chatSessionId}`)}>
-                                        <Text color={"blackAlpha.800"}>{session.title}</Text>
-                                        <Spacer />
-                                        <Trash color="red" onClick={(e) => {
-                                            e.stopPropagation();
-                                            setOpen(true);
-                                        }} />
-                                    </Button>
-                                    <DeleteModal
-                                        open={open}
-                                        title={session.title}
-                                        handleClose={() => setOpen(!open)}
-                                        sessionId={session.chatSessionId} />
-                                </>
-                            ))}
-                        </Stack>
-                    </Stack>
-                </VStack>
-            </Collapsible.Content>
-        </Collapsible.Root>
+                    </VStack>
+                </Collapsible.Content>
+            </Collapsible.Root>
+            <DeleteModal
+                open={open}
+                title={sessions.find(s => (s.chatSessionId === sessionId))?.title || ''}
+                handleClose={() => setOpen(!open)}
+                sessionId={sessionId!} />
+        </>
     );
 }
 
@@ -104,11 +133,17 @@ interface DeleteModalProps {
 const DeleteModal = ({ open, title, sessionId, handleClose }: DeleteModalProps) => {
     const navigate = useNavigate();
 
-    const onDelete = async () => {
+    const onDelete = async (e: React.FormEvent) => {
+        e.preventDefault();
         try {
             const response = await apiClient.delete(`/Chat/session/${sessionId}`);
+            console.log('delete response', response);
             if (response.status === 200) {
+                handleClose();
                 navigate(`/nutriwise/chats`);
+                setTimeout(() => {
+                    window.location.reload(); // Force reload after navigating
+                }, 100);
             }
         } catch (err) {
             console.error(err);
@@ -120,7 +155,8 @@ const DeleteModal = ({ open, title, sessionId, handleClose }: DeleteModalProps) 
             open={open}
             onClose={handleClose}
             aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description">
+            aria-describedby="modal-modal-description"
+        >
             <Box
                 sx={{
                     position: "absolute",
